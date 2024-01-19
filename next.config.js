04a8310ffe8940f19752f2a00f4d64049ed49398
Aurora Kynkor <aurorakynkor@gmail.com>
2024-01-18 21:00:37 -0800

    fixing local

--- next.config.js
+++ next.config.js
@@ -11,12 +11,6 @@ const nextConfig = {
      */
     output: "export",
   
-    /**
-     * Set base path. This is usually the slug of your repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/aurorakynkor.github.io",
   
     /**
      * Disable server-based image optimization. Next.js does not support
