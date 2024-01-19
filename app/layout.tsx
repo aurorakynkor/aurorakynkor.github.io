822c6d890f2fb9e2c5e122ca6ec8e8df0231b3f8
Aurora Kynkor <aurorakynkor@gmail.com>
2024-01-18 20:38:56 -0800

    fixing css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "aurorakynkor.github.io/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
